ac3b9d3b6d6fb757eec98b5edc6a70621b556393
Wecreds <wecredsbr@gmail.com>
2023-05-08 23:07:10 -0300

    interacao com produto.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <script setup>
+import { computed, ref } from 'vue';
 import { produtos } from './_data/produtos';
 import { carrinho } from './_data/carrinho';
 
+function adicionarProduto(produto){
+  const itemCarrinho = carrinho.value.items.find(item => item.id === produto.id);
+  if (itemCarrinho){
+    itemCarrinho.quantidade++;
+    itemCarrinho.valorTotal = itemCarrinho.preco * itemCarrinho.quantidade;
+  } else {
+      const novoItem = {
+        id: produto.id,
+        nome: produto.nome,
+        preco: produto.preco,
+        quantidade: 1
+      };
+      carrinho.value.items.push(novoItem);
+  }
+};
 
+function incrementarItem(item){
+  item.value.quantidade++;
+}
+
+function decrementarItem(item){
+  item.value.quantidade--;
+}
+
+const totalCarrinho = computed(() => {
+  return carrinho.value.items.reduce((total, item) => {
+    return total + item.preco * item.quantidade;
+  }, 0);
+});
 
 </script>
 
 <template>
   <div class="min-h-screen">
-    <div v-for="(item) in produtos" :key="item.id">
-    {{ item.nome }} - R${{ item.preco }}
-
+    <div v-for="produto in produtos" :key="produto.id">
+    {{ produto.nome }} - R$ {{ produto.preco}}
+    <button @click="adicionarProduto(produto)">Adicionar Produto</button>
     </div>
 
-    <div v-for="(index, item) in carrinho" :key="index">
-    {{ item }} : {{ index }}
+    <div v-for="item in carrinho.items" :key="item.id">
+    {{ item.nome }}(s) {{ item.quantidade }} - R${{ item.preco * item.quantidade }}
+    <button @click="incrementarItem(ref(item))">Incrementar Item</button>
+    <br>
+    <button @click="decrementarItem(ref(item))">Decrementar Item</button>
     </div>
-    </div>
+    R${{ totalCarrinho }}
+  </div>
 </template>
--- src/_data/carrinho.js
+++ src/_data/carrinho.js
@@ -2,11 +2,7 @@ import { ref } from 'vue';
 
 export const carrinho = ref({
   items: [
-    {
-      id: 1,
-      nome: 'Camiseta',
-      preco: 49.90
-  },
   ],
-  valorTotal: null
+  
+  valorTotal: 0
 })
